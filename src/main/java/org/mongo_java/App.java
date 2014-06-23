ab84c0eaadad59fbce63ddc3fab4da70bce8f593
Naimdjon <naimdjon@gmail.com>
2014-06-23 21:58:49 +0200

    java mongo

--- src/main/java/org/mongo_java/App.java
+++ src/main/java/org/mongo_java/App.java
@@ -1,13 +1,25 @@
 package org.mongo_java;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
+import java.net.UnknownHostException;
+import java.util.Set;
+
+public class App {
+
+    public static void main(String[] args) throws UnknownHostException {
+        final MongoClient mongoClient = new MongoClient();
+        final DB db = mongoClient.getDB( "testdb" );
+        //listAllCollections(db);
+
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    static void listAllCollections(final DB db) {
+        final Set<String> colls = db.getCollectionNames();
+        for (String s : colls) {
+            System.out.println(s);
+        }
     }
 }
