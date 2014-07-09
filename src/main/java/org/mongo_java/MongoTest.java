5c26792a62562e922f077784c24f94d4dbf0a963
Naimdjon Takhirov <naimdjon@gmail.com>
2014-07-09 11:17:37 +0200

    mongo test

---
+++ src/main/java/org/mongo_java/MongoTest.java
@@ -0,0 +1,36 @@
+package org.mongo_java;
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.Set;
+
+public class MongoTest {
+
+    public static void main(String[] args) throws UnknownHostException {
+        final MongoClient mongoClient = new MongoClient();
+        final DB db = mongoClient.getDB( "test" );
+        DBCollection users = db.getCollection("test");
+        BasicDBObject query = new BasicDBObject();
+        query.put("name",  java.util.regex.Pattern.compile("Hard Working", java.util.regex.Pattern.CASE_INSENSITIVE));
+        DBCursor dbObjects = users.find(query);
+        //listAllCollections(db);
+        printCursor(dbObjects);
+    }
+
+    private static void printCursor(DBCursor dbObjects) {
+        while (dbObjects.hasNext()) {
+            DBObject next = dbObjects.next();
+            System.out.println(next);
+        }
+    }
+
+
+    @SuppressWarnings("UnusedDeclaration")
+    static void listAllCollections(final DB db) {
+        final Set<String> colls = db.getCollectionNames();
+        for (String s : colls) {
+            System.out.println(s);
+        }
+    }
+}
